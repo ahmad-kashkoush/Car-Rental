262aa2ae4759375f0c3a8baa31d952717d7591db
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-08 14:42:57 +0300

    feat(Booking): add form

---
+++ src/components/Booking.jsx
@@ -0,0 +1,110 @@
+import SectionTitle from '@/shared/SectionTitle';
+import { useState } from 'react';
+
+const dropDowns = [
+  {
+    label: 'Select Car type',
+    for: 'car-type',
+    preIcon: 'images/car-type-icon.svg',
+    placeholder: 'Select car type',
+    choices: [
+      'Select car type',
+      'Sedan',
+      'SUV',
+      'Hatchback',
+      'Coupe',
+      'Convertible',
+    ],
+  },
+  {
+    label: 'Pick-up',
+    for: 'pick-up',
+    preIcon: 'images/marker-icon.svg',
+    placeholder: 'Select pick up location',
+    choices: [
+      'Select Pickup location',
+      'New York',
+      'Los Angeles',
+      'Chicago',
+      'Houston',
+      'San Francisco',
+    ],
+  },
+  {
+    label: 'Drop-of',
+    for: 'drop-of',
+    preIcon: 'images/marker-icon.svg',
+    placeholder: 'Select drop off location',
+    choices: [
+      'Select drop off location',
+      'New York',
+      'Los Angeles',
+      'Chicago',
+      'Houston',
+      'San Francisco',
+    ],
+  },
+];
+function Booking() {
+  const [date, setDate] = useState(new Date());
+  // Todo: [] update section padding
+  return (
+    <section className="booking mx-4">
+      <div className="container mx-auto bg-bg-primary px-12 py-8 shadow-md">
+        <SectionTitle className="font-semibold">Book a car</SectionTitle>
+        <form className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+          {dropDowns.map((item) => (
+            <div className="" key={item}>
+              <label className="flex gap-1" htmlFor={item.for}>
+                <img src={item.preIcon} />
+                {item.label}
+              </label>
+              <select
+                name={item.for}
+                className="mt-3 w-full px-4 py-3"
+                id={item.for}
+              >
+                {item.choices.map((option) => (
+                  <option value={option} key={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ))}
+          <div>
+            <label htmlFor="pickup" className="flex gap-1">
+              <img src="images/calendar-icon.svg" alt="pickup" />
+              Pick-up
+            </label>
+            <input
+              type="date"
+              id="pickup"
+              defaultValue={date}
+              //   onChange={setDate((date) => date)}
+              className="mt-3 w-full bg-bg-secondary px-4 py-3"
+            />
+          </div>
+          <div>
+            <label htmlFor="dropoff" className="flex gap-1">
+              <img src="images/calendar-icon.svg" alt="dropoff" />
+              Drop-of
+            </label>
+            <input
+              type="date"
+              id="dropoff"
+              defaultValue={date}
+              //   onChange={setDate((date) => date)}
+              className="mt-3 w-full bg-bg-secondary px-4 py-3"
+            />
+          </div>
+
+          <button className="h-fit w-full place-self-end bg-accent px-4 py-2 font-medium text-white">
+            Search
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
+export default Booking;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import {
+  Booking,
   Hero,
 } from '@/components';
 function Home() {
   return (
     <main className="home w-full grow bg-bg-secondary">
       <Hero /> {/* Implemented */}
+      <Booking /> {/* Implemented, ✏️ need review & refactoring */}
     </main>
   );
 }
