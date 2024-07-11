0966b028b5586982bd8f1c76a4b091b36da1e267
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-11 12:36:51 +0300

    feat(Header): add bg-color on scroll
    - using intersection Observer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Button from '@/shared/Button';
 import { useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
 
 const navLinks = [
@@ -13,41 +14,54 @@ const navLinks = [
 
 function Header() {
   const [checked, setChecked] = useState(false);
-  // TODO: [ ] Add Intersection Observer to change header color, when not on hero section
+  const { ref: containerRef, inView: visible } = useInView({
+    root: null,
+    threshold: 0,
+    rootMargin: '10%',
+  });
+  // TODO: [x] Add Intersection Observer to change header color, when not on hero section
   return (
-    <header className="fixed z-50 w-full">
-      <div className="container mx-auto flex justify-between px-6 py-4">
-        <div className="logo">
-          <Link to={'/'}>
-            <img src="images/logo/logo.png" alt="logo" className="w-34 h-12" />
-          </Link>
-        </div>
-        <label htmlFor="ham-checkbox" className="hamburgerMenu px-6 py-4">
-          <input
-            type="checkbox"
-            id="ham-checkbox"
-            checked={checked}
-            onClick={() => setChecked((c) => !c)}
-          />
-        </label>
-
-        <ul className="flex flex-1 items-center justify-center gap-4 font-medium">
-          {navLinks.map((link) => (
-            <Link
-              to={link.to}
-              key={link.to}
-              onClick={() => setChecked(false)}
-              className="text-title transition-colors duration-300 hover:text-accent"
-            >
-              {link.link}
+    <header ref={containerRef}>
+      <nav
+        className={`relative z-40 ${visible ? '' : '!fixed w-full bg-bg-primary'}`}
+      >
+        <div className="container relative mx-auto flex items-center justify-between px-6 py-4">
+          <div className="logo">
+            <Link to={'/'}>
+              <img
+                src="images/logo/logo.png"
+                alt="logo"
+                className="w-34 h-12"
+              />
             </Link>
-          ))}
-        </ul>
-        <div className="item-center hidden gap-6 min-[1100px]:flex">
-          <Button styleType="transparent">Signup</Button>
-          <Button styleType="primary">Login</Button>
+          </div>
+          <label htmlFor="ham-checkbox" className="hamburgerMenu px-6 py-4">
+            <input
+              type="checkbox"
+              id="ham-checkbox"
+              checked={checked}
+              onChange={() => setChecked((c) => !c)}
+            />
+          </label>
+
+          <ul className="flex flex-1 items-center justify-center gap-4 font-medium">
+            {navLinks.map((link) => (
+              <Link
+                to={link.to}
+                key={link.to}
+                onClick={() => setChecked(false)}
+                className="text-title transition-colors duration-300 hover:text-accent"
+              >
+                {link.link}
+              </Link>
+            ))}
+          </ul>
+          <div className="item-center relative hidden gap-6 min-[1100px]:flex">
+            <Button styleType="transparent">Signup</Button>
+            <Button styleType="primary">Login</Button>
+          </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 }
