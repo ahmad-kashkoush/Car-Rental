a650169ea3d702da85e88129a14d45c0b4771a42
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-13 05:18:32 +0300

    chore(*): setup project
    - setup app routes in App.jsx
    - add assets & images
      - add svg icons, backgrounds, and favicon
      - add react-datepicker
    - setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-function App(){
-    return (
-        <div>
-            App
-        </div>
-    );
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// router pages
+import AppLayout from '@/ui/AppLayout';
+import About from '@/pages/About';
+import Contact from '@/pages/Contact';
+import Home from '@/pages/Home';
+import Models from '@/pages/Models';
+import Team from '@/pages/Team';
+import Testimonials from '@/pages/Testimonials';
+
+const router = createBrowserRouter([
+  {
+    // route wrapper
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/contact', element: <Contact /> },
+      { path: '/models', element: <Models /> },
+      { path: '/testimonials', element: <Testimonials /> },
+      { path: '/team', element: <Team /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
+
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Rubik', 'sans-serif'],
+      'mono': ['Poppins']
+    },
+
+    extend: {
+      colors: {
+        'bg-primary': '#ffffff',
+        'bg-secondary': '#f8f8f8',
+        'accent': '#ff4d30',
+        'title': '#010103',
+        'normal-text': '#706f7b',
+        'black-btn': '#161616',
+        'ham': "#161616",
+        "gray-input":"#ececec"
+      }
+
+
+
+
+    },
   },
   plugins: [],
 }
