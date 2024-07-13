4580d05c836381c86e797aa48de2a57ae07cc001
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-13 05:24:13 +0300

    feat(*): add boiler plates comp

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export { default as AppLayout } from "@/components/AppLayout";
+export { default as Footer } from "@/components/Footer";
+export { default as Header } from "@/components/Header";
+export { default as Hero } from "@/components/Hero";
+export { default as Booking } from "@/components/Booking";
+export { default as Plan } from "@/components/Plan";
+export { default as Pick } from "@/components/Pick";
+export { default as Choose } from "@/components/Choose";
+export { default as TestimonialSection } from "@/components/TestimonialSection";
+export { default as Banner } from "@/components/Banner";
+export { default as Faq } from "@/components/Faq";
+export { default as Download } from "@/components/Download";
